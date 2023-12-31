675489c9e876c9b2024a52de373a99cb6cd332db
ZeroKnight05 <110831750+ZeroKnight05@users.noreply.github.com>
2023-12-31 21:26:51 +0700

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const multer = require('multer'); // For handling file uploads
-
+const multer = require('multer');
 const app = express();
 
-// Multer storage configuration
+// Multer storage configuration for file uploads
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads/') // Destination folder for uploads
+        cb(null, 'uploads/'); // Destination folder for uploads
     },
     filename: function (req, file, cb) {
         cb(null, Date.now() + '-' + file.originalname); // Unique filename
@@ -24,6 +23,19 @@ app.post('/upload', upload.single('fileInput'), (req, res) => {
     }
     // Save file details to database, etc.
     return res.status(200).send('File uploaded successfully.');
+});
+
+// POST endpoint for sending messages with file uploads
+app.post('/send-message', upload.single('fileInput'), (req, res) => {
+    const textMessage = req.body.message; // Text message
+    const file = req.file; // Uploaded file details (if any)
+
+    // Save message to database including text, file details, and user references
+    // Example: MongoDB database interaction
+    // Message.create({ text: textMessage, file: file, sender: req.user.id, receiver: receiverId });
+
+    // Return success response
+    return res.status(200).send('Message sent successfully.');
 });
 
 // Start server
